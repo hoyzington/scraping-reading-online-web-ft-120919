8bb7395cd2c18dcc2b40383ab9faf1c887c39623
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 20:56:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,8 +9,7 @@ doc.css(".headline-26OIBN")
 
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4").children
 
+
+
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4
 
-#\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R
-
-courses.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")
