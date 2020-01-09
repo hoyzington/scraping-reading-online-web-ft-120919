3bb2eafdc5f63cd7a495570dbea480713aa8cca5
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 20:07:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,3 +10,4 @@ doc.css(".headline-26OIBN")
 doc.css(".title-oE5vT4").text
 
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4
+
