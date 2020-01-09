fbf95f2ddda2d00be2ae2cfd9bd5df20a9364b37
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 20:12:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ doc = Nokogiri::HTML(html)
 
 doc.css(".headline-26OIBN")
 
-doc.css(".content-3OTK_R.title-oE5vT4").text
+doc.css(".text-4GLMvr.title-oE5vT4").text
 
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4
 
