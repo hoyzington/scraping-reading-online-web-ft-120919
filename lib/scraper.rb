aacdf58dd4f0909fecf24d40241a2a9f2892b087
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 19:56:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,6 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
+doc.css()
+
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4
