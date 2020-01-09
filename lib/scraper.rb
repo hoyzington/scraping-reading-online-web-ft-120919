d010f2f3d79a51cda3bf602a76938d424e3a5fa2
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 20:25:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,3 +12,5 @@ doc.css("#a778efd-1685-5ec6-9e5a-0843d6a88b7b .text-4GLMvr.title-oE5vT4").text
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4
 
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R
+
+doc.css("32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4")
