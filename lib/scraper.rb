d6cd686a262a3bb2b1b57cfd8a175ee8395ff0fb
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 20:44:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,10 +7,10 @@ doc = Nokogiri::HTML(html)
 
 doc.css(".headline-26OIBN")
 
-doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4").text
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4").text
 
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4
 
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R
 
-.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])
+courses.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")
